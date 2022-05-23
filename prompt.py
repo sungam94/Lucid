1cfa7dac199c9c4debc127419803d5f555f36667
mag <magnus.kaffenberger@gmail.com>
2022-05-24 01:36:41 +0200

    init

---
+++ prompt.py
@@ -0,0 +1,25 @@
+import requests
+from torch import nn
+
+
+def fetch(url_or_path):
+    if str(url_or_path).startswith("http://") or str(url_or_path).startswith(
+        "https://"
+    ):
+        r = requests.get(url_or_path)
+        r.raise_for_status()
+        fd = io.BytesIO()
+        fd.write(r.content)
+        fd.seek(0)
+        return fd
+    return open(url_or_path, "rb")
+
+
+def parse_prompt(prompt):
+    if prompt.startswith("http://") or prompt.startswith("https://"):
+        vals = prompt.rsplit(":", 2)
+        vals = [vals[0] + ":" + vals[1], *vals[2:]]
+    else:
+        vals = prompt.rsplit(":", 1)
+    vals = vals + ["", "1"][len(vals) :]
+    return vals[0], float(vals[1])
